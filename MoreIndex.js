ea8be26cd967d47c1b1727fd8bcba6fd3828dda2
Jurcix <pakalnyte.jurate@gmail.com>
2016-06-27 14:52:57 +0300

    iCanToo

--- MoreIndex.js
+++ MoreIndex.js
@@ -1,10 +1,17 @@
 /**
  * Created by Richard on 6/28/2016.
  */
-var HelloWorld = "Goodbye World";
+var HelloWorld = prompt("Say goodbye/hello world").toLowerCase();
 console.log(HelloWorld);
-if(HelloWorld="Goodbye World")
+if(HelloWorld==="goodbye world")
 {
     HelloWorld = "Just kidding, HI world!";
     console.log(HelloWorld);
 }
+else
+{
+    HelloWorld = "Hello world!";
+    console.log(HelloWorld);
+}
+
+console.log("Don't think you're the only one who can play this game!");
