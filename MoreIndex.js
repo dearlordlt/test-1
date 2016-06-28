9f744d78afa5d82aca6950c009d257aa76e6c3be
RicardasFaturovas <ricardas.faturovas@gmail.com>
2016-06-28 02:32:04 -0700

    Hello world+more packages

---
+++ MoreIndex.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Richard on 6/28/2016.
+ */
+var HelloWorld = "Goodbye World";
+console.log(HelloWorld);
+if(HelloWorld="Goodbye World")
+{
+    HelloWorld = "Just kidding, HI world!";
+    console.log(HelloWorld);
+}
